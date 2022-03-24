b888542600639d6e0c0c7bebd072c60308a1256b
Noah Husby <noahhusby@gmail.com>
2022-03-23 20:00:52 -0500

    Added ID field to table

--- src/main/resources/public/js/main.js
+++ src/main/resources/public/js/main.js
@@ -9,7 +9,7 @@ $.ajax({
     dataType: 'json',
     success: function (data) {
         for (var i = 0; i < data.length; i++) {
-            var row = $('<tr><td class="column1">' + data[i].date + '</td><td class="column2">' + data[i].size + '</td><td class="column3"><ul>' +
+            var row = $('<tr><td class="column1">' + data[i].id + '</td><td class="column2">' + data[i].date + '</td><td class="column3">' + data[i].size + '</td><td class="column4"><ul>' +
                     '<li><a href="/data.json" download target="_blank" title="Download Excel Sheet"><span class="material-icons-outlined">download</span></a></li>' +
                     '<li><a title="Download All as ZIP"><span class="material-icons-outlined">folder_zip</span></button></a></li>' +
                     '<li><a title="View Log"><span class="material-icons-outlined">launch</span></a></li>'
